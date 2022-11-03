70a6b09e34ee7fb63980391e50293bfd4fc78802
Jasperi Sivenius <jmmsiv@utu.fi>
2022-11-03 10:15:05 +0200

    Updated version of interface

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -6,11 +6,29 @@ using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     // Play, or as we call it, launch
-    public void Launch()
+    public void LaunchForest()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+        Debug.Log("Forest Scene Loaded.");
     }
 
+    public void LaunchMountain()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +2);
+        Debug.Log("Mountain Scene loaded.");
+    }
+
+    public void LaunchTurku()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +0);
+        Debug.Log("Turku Scene unfortunately not implemented yet.");
+    }
+
+    public void LaunchSea()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +0);
+        Debug.Log("Sea Scene unfortunately not implemented yet.");
+    }
     // Quit
     public void Quit()
     {
