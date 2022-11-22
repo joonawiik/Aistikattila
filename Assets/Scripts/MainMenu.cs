39c6283b545b88c086e893422ffb3d4307e670b3
Jasperi Sivenius <jmmsiv@utu.fi>
2022-11-22 13:29:46 +0200

    Updating interface and trying out the multiscene display

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -29,10 +29,31 @@ public class MainMenu : MonoBehaviour
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +0);
         Debug.Log("Sea Scene unfortunately not implemented yet.");
     }
+
+    public void LaunchWinterForest()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +0);
+        Debug.Log("Winter Forest Scene unfortunately not implemented yet.");
+    }
+
+    public void LaunchMovingTrain()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +0);
+        Debug.Log("Moving Train Scene unfortunately not implemented yet.");
+    }
+
     // Quit
     public void Quit()
     {
         Application.Quit();
         Debug.Log("Quitting happened.");
+    }
+
+    public void ReturnToMainMenu()
+    {
+        //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1);
+        SceneManager.LoadScene("Interface");
+        Display.displays[0].Activate();
+        Debug.Log("Returned to main menu interface.");
     }
 }
