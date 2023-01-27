a4e26393848a5055cee22a10d40a526a539f286a
cherr66 <cherchen999@gmail.com>
2023-01-27 23:44:14 +0200

    day night switch
    + add switch manager, preset, etc
    * modify the forest scene & some basic settings

---
+++ Assets/Scripts/DayNightSwitch/DayNightSwitchManager.cs
@@ -0,0 +1,174 @@
+using UnityEngine;
+
+
+[ExecuteAlways]
+public class DayNightSwitchManager : MonoBehaviour
+{
+    //Scene References
+    [SerializeField] private DayNightSwitchPreset Preset;
+    [SerializeField] private Material Skybox;
+    [SerializeField] private Light DirectionalLight;
+    [SerializeField] private LightingSettings lightingSetting;
+    [SerializeField] private GameObject particleRoot;
+    //Variables
+    [SerializeField, Range(0, 24)] private float TimeOfDay;
+    //[SerializeField] private float AnimationDuration = 3.0f;
+    
+
+    private void Update()
+    {
+        if (Preset == null)
+            return;
+
+        if (Application.isPlaying)
+        {
+            // Animation, not needed
+            // TimeOfDay += Time.deltaTime;
+            // TimeOfDay %= 24; //Modulus to ensure always between 0-24
+            // UpdateLighting(TimeOfDay / 24f);
+        }
+        else
+        {
+            UpdateLighting(TimeOfDay / 24f);
+        }
+    }
+
+    private void UpdateLighting(float timePercent)
+    {
+        bool isMorning = (timePercent <= 0.5f)? true: false;
+
+        //Set environment ambient color & shadow color
+        RenderSettings.ambientLight = Preset.environment.environmentLightColor.Evaluate(timePercent);
+        RenderSettings.subtractiveShadowColor = Preset.environment.shadowColor.Evaluate(timePercent);
+        
+        //Set skybox & exposure & rotation
+        RenderSettings.skybox.SetColor("_Tint", Preset.skybox.color.Evaluate(timePercent));
+        RenderSettings.skybox.SetFloat("_Exposure", (isMorning)? 
+            Mathf.Lerp(Preset.skybox.midNightExposure, Preset.skybox.midDayExposure, timePercent * 2.0f):
+            Mathf.Lerp(Preset.skybox.midDayExposure, Preset.skybox.midNightExposure, (timePercent - 0.5f) * 2.0f));
+        RenderSettings.skybox.SetFloat("_Rotation", Mathf.Lerp(0, 360, timePercent));
+
+        // Set the directionalLight
+        if (DirectionalLight != null)
+        {
+            DirectionalLight.color = Preset.directionalLight.color.Evaluate(timePercent);
+            DirectionalLight.intensity = (isMorning)?
+                Mathf.Lerp(Preset.directionalLight.midNightIntensity, Preset.directionalLight.midDayIntensity, timePercent * 2.0f):
+                Mathf.Lerp(Preset.directionalLight.midDayIntensity, Preset.directionalLight.midNightIntensity, (timePercent - 0.5f) * 2.0f);
+            DirectionalLight.transform.localRotation = Quaternion.Euler(new Vector3((timePercent * 360f) - 90f, 170f, 0));
+        }
+
+        // Set fog when fog is on
+        if (RenderSettings.fog == true)
+        {
+            RenderSettings.fogColor = Preset.fog.color.Evaluate(timePercent);
+            RenderSettings.fogDensity = (isMorning)?
+                Mathf.Lerp(Preset.fog.midNightDensity, Preset.fog.midDayDensity, timePercent * 2.0f):
+                Mathf.Lerp(Preset.fog.midDayDensity, Preset.fog.midNightDensity, (timePercent - 0.5f) * 2.0f);
+        }
+
+        // particles
+        if(particleRoot != null && (timePercent <= 0.2 || timePercent >= 0.8))
+        {
+            foreach(var child in particleRoot.GetComponentsInChildren<ParticleSystem>())
+            {
+                //..
+            }
+        }
+
+    }
+
+    //Called when 'Night' is selected
+    public void DayToNight()
+    {
+        UpdateLighting(12 / 24f);
+        TimeOfDay = 24f;
+        if(particleRoot != null)
+        {
+            particleRoot.SetActive(true);
+        }
+    }
+
+    //Called when 'Day' is selected
+    public void NightToDay()
+    {
+        TimeOfDay = 0f;
+        UpdateLighting(24 / 24f);
+        if(particleRoot != null)
+        {
+            particleRoot.SetActive(false);
+        }
+        TimeOfDay = 12f;
+    }
+
+
+    // Editor-only function that Unity calls when the script is loaded or a value changes in the Inspector.
+    private void OnValidate()
+    {
+        // Make sure the DirectionalLight is not missing
+        if (DirectionalLight == null)
+        {
+            //Search for lighting tab sun
+            if (RenderSettings.sun != null)
+            {
+                DirectionalLight = RenderSettings.sun;
+            }
+            //Search scene for light that fits criteria (directional)
+            else
+            {
+                Light[] lights = GameObject.FindObjectsOfType<Light>();
+                foreach (Light light in lights)
+                {
+                    if (light.type == LightType.Directional)
+                    {
+                        DirectionalLight = light;
+                        return;
+                    }
+                }
+        }
+        }
+    
+        // Shadow color only works with MixedLightingMode.Subtractive
+        if(lightingSetting != null)
+        {
+            lightingSetting.mixedBakeMode = MixedLightingMode.Subtractive;
+        }
+
+        // Set Environment Lighting Source as 'Color' so that ambient color works
+        RenderSettings.ambientMode = UnityEngine.Rendering.AmbientMode.Flat;
+
+        //Make Sure skybox is not missing.
+        if (RenderSettings.skybox == null)
+        {
+            RenderSettings.skybox = Skybox;
+        }
+
+        //Make Sure the particle Root has a ParticleSystem component so as to activate all particles when focused.
+        if (particleRoot != null)
+        {
+            var ps = particleRoot.GetComponent<ParticleSystem>();
+            if(ps == null)
+            {
+                ps= particleRoot.AddComponent<ParticleSystem>();
+            }
+            
+            // make sure the emission/shape is disabled so that it does not lag the compiling
+            var e = ps.emission;
+            e.enabled = false;
+            var s = ps.shape;
+            s.enabled = false;
+        }
+    }
+
+    private void Start()
+    {
+        // TODO: check interface parameter
+        TimeOfDay = 12f;
+        OnValidate();
+    }
+
+    private void onAwake()
+    {
+        //Todo: read from current rendering setting onAwake?
+    }
+}
---
+++ Assets/Scripts/DayNightSwitch/DayNightSwitchPreset.cs
@@ -0,0 +1,50 @@
+using System;
+using UnityEngine;
+
+
+[Serializable]
+[CreateAssetMenu(fileName ="DayNightSwitch Preset", menuName ="Scriptables/DayNightSwitch Preset",order =1)]
+public class DayNightSwitchPreset : ScriptableObject
+{    
+    public GeneralEnvironmentSetting environment;
+    public SkyboxSetting skybox;
+    public DirectionalLightSetting directionalLight;
+    public FogSetting fog;
+
+    void onGUI(){}
+}
+
+[Serializable]
+public class GeneralEnvironmentSetting
+{
+    public Gradient shadowColor;
+    //Should check the source is color
+    public Gradient environmentLightColor;
+}
+
+[Serializable]
+public class SkyboxSetting
+{
+    public Gradient color;
+    public float midDayExposure = 1.0f;
+    public float midNightExposure = 1.0f;
+    // public float midDayRotation = 1.0f;
+    // public float midNightRotation = 1.0f;
+}
+
+[Serializable]
+public class DirectionalLightSetting
+{
+    public Gradient color;
+    public float midDayIntensity;
+    public float midNightIntensity;
+    // public float IndirectMultiplier;
+}
+
+[Serializable]
+public class FogSetting
+{
+    public Gradient color;
+    public float midDayDensity = 0.0f;
+    public float midNightDensity = 0.01f;
+}
