6b890946d69166b0cba369bbf3dd755ef33f145a
Jasperi Sivenius <jmmsiv@utu.fi>
2023-02-13 18:04:27 +0200

    Updating the interface, adding the adjustment settings box and fixing bugs and fonts

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -1,7 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using System.Linq;
 
 public class MainMenu : MonoBehaviour
 {
@@ -11,6 +13,17 @@ public class MainMenu : MonoBehaviour
     public GameObject mainMenu;
     public GameObject managerMenu; 
     public GameObject newMenu;
+
+    public GameObject forestAdjustmentBox;
+    public ToggleGroup forestTimeOfDay;
+    public ToggleGroup forestWeather;
+    public ToggleGroup forestSound;
+    public enum ForestTimeOfDayState { Day, Night };
+    public ForestTimeOfDayState currentForestTimeOfDay;
+    public enum ForestWeatherState { Sunny, Rainy };
+    public ForestWeatherState currentForestWeather;
+    public enum ForestSoundState { On, Off };
+    public ForestSoundState currentForestSound;
 
     void Awake()
     {
@@ -66,8 +79,20 @@ public class MainMenu : MonoBehaviour
     // Play, or as we call it, launch
     public void LaunchForest()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
-        Debug.Log("Forest Scene Loaded.");
+        Debug.Log(currentForestTimeOfDay);
+        Debug.Log(currentForestWeather);
+        Debug.Log(currentForestSound);
+
+        // If the adjustment box is active, the scene won't be launched
+        if (forestAdjustmentBox.active)
+        {
+            Debug.Log("The forest adjustment box is active, it needs to be closed and the changed settings saved before launching the scene.");
+        }
+        else
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +1);
+            Debug.Log("Forest Scene Loaded.");
+        }
     }
 
     public void LaunchMountain()
@@ -98,6 +123,47 @@ public class MainMenu : MonoBehaviour
     {
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex +0);
         Debug.Log("Moving Train Scene unfortunately not implemented yet.");
+    }
+
+    // Called upon clicking the Adjust button in the Forest box
+    public void AdjustForest()
+    {
+        Debug.Log("Adjusting Forest scenery settings.");
+        forestAdjustmentBox.SetActive(true);
+    }
+
+    // Called upon clicking the Apply button in the Forest adjustment box
+    public void StopAdjustingForest()
+    {
+        Toggle selectedTimeOfDay = forestTimeOfDay.ActiveToggles().FirstOrDefault();
+        if (selectedTimeOfDay.name.ToString() == "Day")
+        {
+            currentForestTimeOfDay = ForestTimeOfDayState.Day;
+        }
+        else if (selectedTimeOfDay.name.ToString() == "Night")
+        {
+            currentForestTimeOfDay = ForestTimeOfDayState.Night;
+        }
+        Toggle selectedWeather = forestWeather.ActiveToggles().FirstOrDefault();
+        if (selectedWeather.name.ToString() == "Sunny")
+        {
+            currentForestWeather = ForestWeatherState.Sunny;
+        }
+        else if (selectedWeather.name.ToString() == "Rainy")
+        {
+            currentForestWeather = ForestWeatherState.Rainy;
+        }
+        Toggle selectedSound = forestSound.ActiveToggles().FirstOrDefault();
+        if (selectedSound.name.ToString() == "On")
+        {
+            currentForestSound = ForestSoundState.On;
+        }
+        else if (selectedSound.name.ToString() == "Off")
+        {
+            currentForestSound = ForestSoundState.Off;
+        }
+        Debug.Log(selectedTimeOfDay);
+        forestAdjustmentBox.SetActive(false);
     }
 
     // Quit
